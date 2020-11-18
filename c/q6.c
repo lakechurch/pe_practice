7f0edb2b3d5ddb4b79f579804979da9e9e84b912
lakechurch <curtis@meerkerk.ca>
2020-11-18 08:31:11 -0800

    Completed Q6 in c

---
+++ c/q6.c
@@ -0,0 +1,25 @@
+/*
+ * Problem 6: Sum square difference
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ *  1^2 + 2^2 + 3^2 + ... + 10^2 = 385
+ * The square of the sum of the first ten natural numbers is,
+ *  (1+2+3+...+10)^2 = (55)^2 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+ *  3025-385=2640
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+#include <stdio.h>
+
+int main(void) {
+  unsigned long long int sum_of_squares=0, square_of_sum=0;
+
+  for (int i = 1; i <= 100; i++) {
+    sum_of_squares += i*i;
+    square_of_sum += i;
+  }
+  square_of_sum *= square_of_sum;
+
+  printf("%llu %llu %llu\n", square_of_sum, sum_of_squares, square_of_sum-sum_of_squares);
+}
